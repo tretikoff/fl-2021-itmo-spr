587cb91857ccabe969b86f374d2ca06769900006
tretikoff <tretikov.konstantin@studiomobile.ru>
2021-03-03 15:52:33 +0300

    Add 1, 2

--- src/Parser/Common.hs
+++ src/Parser/Common.hs
@@ -1,8 +1,8 @@
 module Parser.Common where
 
 import Data.Char (isDigit, isAlpha)
-import Parser.Combinators ( Parser, satisfy )
-import Control.Applicative (some)
+import Parser.Combinators ( Parser, satisfy, symbol)
+import Control.Applicative (some, Alternative((<|>)) )
 
 digit :: Parser String Char
 digit = satisfy isDigit
@@ -12,6 +12,12 @@ digit' = satisfy (\x -> isDigit x && x /= '0')
 
 alpha :: Parser String Char
 alpha = satisfy isAlpha
+
+opBr = symbol '('
+clBr = symbol ')'
+
+underscore :: Parser String Char
+underscore = symbol '_'
 
 -- number :: not empty, consists of digits
 number :: Parser String Int
@@ -29,9 +35,17 @@ uberExpr :: [(Parser i op, Associativity)]
          -> Parser i ast
          -> (op -> ast -> ast -> ast)
          -> Parser i ast
-uberExpr x el_p createBinAst = local x where
-    local ((p, ass) : xs) = case ass of
-        LeftAssoc ->
-        RightAssoc ->
-        NoAssoc ->
-    local [] = el_p
+uberExpr ((parse, ass): parsers) elemParser createAst = local ass <|> parseRest where
+    parseRest = uberExpr parsers elemParser createAst
+    local LeftAssoc = parseAssoc go where
+        go expr ((op, opExpr): opExprs) = go (createAst op expr opExpr) opExprs
+        go e [] = e
+    local RightAssoc = parseAssoc go where
+        go expr ((op, opExpr): opExprs) = createAst op expr (go opExpr opExprs)
+        go e [] = e
+    local NoAssoc = flip createAst <$> parseRest <*> parse <*> parseRest
+    parseAssoc f = do
+        rest <- parseRest
+        onSplitted <- some ((,) <$> parse <*> parseRest)
+        return $ f rest onSplitted
+uberExpr [] elemParser createAst = elemParser
--- src/Parser/Infix.hs
+++ src/Parser/Infix.hs
@@ -2,11 +2,34 @@
 module Parser.Infix where
 
 import Expr ( Expr )
-import Parser.Combinators ( Parser, eof, whitespaces, parseEither )
+import Parser.Combinators
+import Parser.Common
+import Control.Applicative ( Alternative((<|>)), many )
+import Expr ( Expr(..), toOp )
 
 parse :: String -> Either String Expr
-parse =
-  parseEither (whitespaces *> parseExpr <* whitespaces <* eof)
+parse = parseEither (whitespaces *> parseExpr <* whitespaces <* eof)
+
+pow1  = toOp <$> word "^"
+power = (pow1, RightAssoc)
+
+mult1  = toOp <$> word "*"
+div1  = toOp <$> word "/"
+mulDiv = ((mult1 <|> div1), LeftAssoc)
+sum1  = toOp <$> word "+"
+minus1 = toOp <$> word "-"
+sumMin = ((sum1 <|> minus1), LeftAssoc)
+
+eq = toOp <$> word "=="
+neq = toOp <$> word "/="
+leq = toOp <$> word "<="
+lt = toOp <$> word "<"
+geq = toOp <$> word ">="
+gr = toOp <$> word ">"
+equality = ((eq <|> neq  <|> leq <|> lt <|> geq <|> gr), NoAssoc)
+
+ident = (:) <$> (alpha <|> underscore) <*> many (alpha <|> digit <|> underscore)
 
 parseExpr :: Parser String Expr
-parseExpr = undefined
+parseExpr = uberExpr [equality, sumMin, mulDiv, power] (whitespaces *> local <* whitespaces) BinOp where
+    local = Num <$> number <|> Ident <$> ident <|> brackets opBr clBr parseExpr
